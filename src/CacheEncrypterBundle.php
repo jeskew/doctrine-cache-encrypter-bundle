46844c4535a843fa5123c05654286d8471e90246
Jonathan Eskew <eskewj@amazon.com>
2016-01-04 00:58:40 -0800

    Update version internally

--- src/CacheEncrypterBundle.php
+++ src/CacheEncrypterBundle.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
 class CacheEncrypterBundle extends ContainerAware implements BundleInterface
 {
-    const VERSION = '0.0.1';
+    const VERSION = '1.1.0';
 
     public function build(ContainerBuilder $container)
     {
